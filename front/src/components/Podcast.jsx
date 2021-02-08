71fb9c96db396933036d9592589f609597e8739c
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-02-08 15:35:53 -0500

    Episode page: Back to homepage

--- front/src/components/Podcast.jsx
+++ front/src/components/Podcast.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import { Button, Item } from 'semantic-ui-react';
 import Rating from './Rating';
 
-const Podcast = ({ podcast }) => {
+const Podcast = ({ podcast, image }) => {
   return (
-    <Item.Group divided>
-      <Item>
-        <Item.Image src='logo192.png' size='small' />
-        <Item.Content>
-          <Item.Header as='a'>{podcast.title}</Item.Header>
-          <Item.Meta>{podcast.date}</Item.Meta>
-          <Item.Description>{podcast.description}</Item.Description>
-          <Item.Extra>
-            <Button icon='play' floated='right' basic circular color='violet' />
-          </Item.Extra>
-          <Rating value={podcast.rating} text={podcast.numReviews} />
-        </Item.Content>
-      </Item>
-    </Item.Group>
+    <Item>
+      <Item.Image src={image} size='small' />
+      <Item.Content>
+        <Item.Header as='a'>{podcast.title}</Item.Header>
+        <Item.Meta>{podcast.date}</Item.Meta>
+        <Item.Description>{podcast.description}</Item.Description>
+        <Item.Extra>
+          <Button icon='play' floated='right' basic circular color='violet' />
+        </Item.Extra>
+        <Rating value={podcast.rating} text={podcast.numReviews} />
+      </Item.Content>
+    </Item>
   );
 };
 
--- front/src/screen/PodcastScreen.jsx
+++ front/src/screen/PodcastScreen.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { Item, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
 import Podcast from '../components/Podcast';
 import episodes from '../tmp/episodes';
+
+import products from '../tmp/products';
 
 const PodcastScreen = ({ match }) => {
   // Note to myself -> Be careful here for type of _id and id
   let podcast = episodes.find((p) => p._id === Number(match.params.id));
 
+  // what is the best?
+  let image = products.find((p) => p._id === match.params.id).img;
+
   return (
     <>
-      {podcast.episodes.map((p) => (
-        <Podcast key={p._eid} podcast={p} />
-      ))}
+      <Button icon='left chevron' as={Link} to='/'>
+        <span>Back</span>
+      </Button>
+      <Item.Group divided>
+        {podcast.episodes.map((p) => (
+          <Podcast key={p._eid} podcast={p} image={image} />
+        ))}
+      </Item.Group>
     </>
   );
 };
